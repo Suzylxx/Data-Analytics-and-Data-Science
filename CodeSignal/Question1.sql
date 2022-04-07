eef84a4d49f35a149ca3074cb289c759495b3ed8
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-04-07 16:43:23 -0400

    Delete CodeSignal directory

--- CodeSignal/Question1.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE solution()
-BEGIN
-	/* Write your SQL here. Terminate each statement with a semicolon. */
-	SELECT project_name, team_lead, income FROM Projects
-	order by internal_id ASC;
-END
