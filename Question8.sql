cf2ff4c98653bcb183d5f0d8cbaa4645358af149
Suzy Liu <43913437+Suzylxx@users.noreply.github.com>
2022-04-12 14:35:28 -0400

    Delete Question8.sql

--- Question8.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE solution()
-BEGIN
-	/* Write your SQL here. Terminate each statement with a semicolon. */
-	SELECT a.name FROM (
-		SELECT name FROM leaderboard
-		ORDER BY score DESC LIMIT 5 OFFSET 3
-	) AS a;
-END
